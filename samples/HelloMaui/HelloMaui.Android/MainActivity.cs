6223d1020c5fb76426a4c0b918eb6355e3b1582e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-02-10 09:45:38 -0600

    Android project builds!

--- samples/HelloMaui/HelloMaui.Android/MainActivity.cs
+++ samples/HelloMaui/HelloMaui.Android/MainActivity.cs
@@ -14,15 +14,8 @@ namespace HelloMaui.Droid
         {
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
