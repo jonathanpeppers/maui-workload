5413aa5b9bfef210b7cbdfc58942582bfd34bdde
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-02-18 16:30:48 -0600

    WIP dogfood.sh for VS Mac

---
+++ scripts/dogfood.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -ue
+
+SCRIPTDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VS=/Applications/Visual\ Studio.app/
+SLN=$(realpath $SCRIPTDIR/../samples/samples.sln)
+
+usage()
+{
+  echo "Settings:"
+  echo "  --vs     Path to Visual Studio for Mac, defaults to '$VS'."
+  echo "  --sln    Path to a project or solution to open, defaults to '$SLN'."
+}
+
+firstArgumentChecked=0
+
+while [[ $# > 0 ]]; do
+  opt="$(echo "${1/#--/-}" | tr "[:upper:]" "[:lower:]")"
+
+  if [[ $firstArgumentChecked -eq 0 && $opt =~ ^[a-zA-Z.+]+$ ]]; then
+    if [ $opt == "help" ]; then
+      usage
+      exit 0
+    fi
+
+    shift 1
+    continue
+  fi
+
+  firstArgumentChecked=1
+
+  case "$opt" in
+     -help|-h|-\?|/?)
+      usage
+      exit 0
+      ;;
+
+     -vs)
+      VS=$2
+      shift 2
+      ;;
+
+     -sln)
+      SLN=$2
+      shift 2
+      ;;
+
+      *)
+      shift 1
+      ;;
+  esac
+done
+
+echo "Opening $SLN..."
+
+DOTNET_INSTALL_DIR=$SCRIPTDIR/bin/dotnet/ \
+    DOTNET_ROOT=$DOTNET_INSTALL_DIR \
+    DOTNET_MSBUILD_SDK_RESOLVER_CLI_DIR=$DOTNET_INSTALL_DIR \
+    DOTNET_MULTILEVEL_LOOKUP=0 \
+    DOTNET_SKIP_FIRST_TIME_EXPERIENCE=0 \
+    PATH=DOTNET_ROOT:$PATH \
+    open -n "$VS" "$SLN"
